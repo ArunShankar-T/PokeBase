0aa1d14c48b5f39ff604ef31e1c676ce83af5bc3
Arun Shankar Thangavel <arunshankar14995@gmail.com>
2022-04-15 03:22:09 +0530

    Added Initial Ui of Grid View with hard coded values

--- lib/view/pokemon_list_page.dart
+++ lib/view/pokemon_list_page.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
+import '../utils/app_strings.dart';
 
 class PokemonListPage extends StatelessWidget {
   const PokemonListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      const SizedBox(height: 80),
+      const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Text(AppStrings.APP_TITLE,
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 24)))),
+      const SizedBox(height: 20),
+      Expanded(
+        child: GridView.builder(
+          itemCount: 9,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+                color: Colors.black12,
+                child: Stack(children: [
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                            child: SvgPicture.network(
+                                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13$index.svg",
+                                height: 100,
+                                width: 100,
+                                placeholderBuilder: (BuildContext context) =>
+                                    Container(
+                                        padding: const EdgeInsets.all(60.0),
+                                        child:
+                                            const CircularProgressIndicator()))),
+                        Text("Ditto",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                        const SizedBox(height: 10)
+                      ],
+                    ),
+                  ),
+                  Transform.translate(
+                      offset: const Offset(145, 10),
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            boxShadow: [BoxShadow(color: Colors.black12)],
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Center(
+                            child: Image.asset("assets/images/pokemon_ball.png",
+                                width: 30, height: 30, fit: BoxFit.cover)),
+                      ))
+                ]));
+          },
+        ),
+      )
+    ]));
   }
 }
